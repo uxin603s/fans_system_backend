a3839581abf022d87a6f712ef5d2fa9a45790434
uxin603s <uxin603s@gmail.com>
2016-12-09 18:29:44 +0800

    update

--- FansList.php
+++ FansList.php
@@ -1,5 +1,20 @@
 <?php
 class FansList{
-	use CURD;
-	public static $filter_field_arr=["id","fb_id","name"];
+	use CRUD;
+	public static $table="fans_list";
+	public static $filter_field_arr=["id","fb_id","name","status","comment"];
+	
+	
+	public static function update_online($arg){
+		$url="";
+		$url.="https://graph.facebook.com?fields=fan_count,name,id";
+		$url.="&access_token";
+		$url.="&ids=";
+		
+		$ids=$arg['ids'];
+		foreach($ids as $id){
+			
+		}
+		return json_decode(file_get_contents($url));
+	}
 }
